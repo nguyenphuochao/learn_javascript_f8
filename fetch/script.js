de7badd5b9f1dce92a7a3e298eef340915f5586e
ThanhTT <thanhtt@splus-software.com.vn>
2025-01-09 21:50:20 +0700

    m

---
+++ fetch/script.js
@@ -0,0 +1,21 @@
+
+
+var postUrl = 'https://jsonplaceholder.typicode.com/posts';
+
+fetch(postUrl)
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (posts) {
+        var htmls = posts.map(function (post) {
+            return `
+                <li>
+                    <h3>${post.title}</h3>
+                    <p>${post.body}</p>
+                </li>
+            `;
+        });
+
+        var html = htmls.join('');
+        document.querySelector("#post-block").innerHTML = html;
+    })
