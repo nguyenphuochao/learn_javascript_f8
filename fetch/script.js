b8c4644b3fe902e2865d15cd67704505ac4990c9
Nguyễn Phước Hảo <nguyenphuochao123@gmail.com>
2025-08-14 23:58:14 +0700

    docs: fetch

--- fetch/script.js
+++ fetch/script.js
@@ -1,10 +1,8 @@
-
-
 var postUrl = 'https://jsonplaceholder.typicode.com/posts';
 
 fetch(postUrl)
     .then(function (response) {
-        return response.json()
+        return response.json(); // JSON.parse: JSON -> javascript types
     })
     .then(function (posts) {
         var htmls = posts.map(function (post) {
